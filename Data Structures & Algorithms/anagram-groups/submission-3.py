2c2bf9fe4bf30790ed64f12784f9ba50e36b9e62
Parv Patodia <patodia.pa@northeastern.edu>
2026-08-28 23:00:29 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = tuple(sorted(word))     #<turn word into its canonical signature>
+            groups[key].append(word)
+
+        return list(groups.values())
+
+
+
+        
